d917b6dc87b1e51cffdda803574c38f0db410a8d
Isaac Hall and Ryan McLean <ryan@>
2019-01-29 11:42:29 -0800

    add second form for deposit & withdrawl money. add new submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
+var account = 0;
+
 function NewAccount(name, balance) {
   this.name = name;
-  this.balanace = 0
+  this.balance = balance;
 }
 
-NewAccount.prototype.deposit = function(deposit) {
-  return balance += deposit;
+NewAccount.prototype.newMoney = function(deposit) {
+  account.balance += deposit;
 }
 
-NewAccount.prototype.withdrawal = function(withdrawal) {
-  return balance -= withdrawl;
+NewAccount.prototype.loseMoney = function(withdrawal) {
+  account.balance -= withdrawal;
 }
 
 $(document).ready(function() {
-  $("#initialDepositButton").submit(function(event) {
+  $("#withdrawal").val("0");
+  $("#deposit").val("0");
+  $(".formGroup").submit(function(event) {
     event.preventDefault()
-    var name = $("#clientName").val();
-    var initialDeposit = parseInt($("#initialDeposit").val());
-    console.log(name);
-    console.log(initialDeposit);
-  })
+    var name = $("input#clientName").val();
+    var balance = parseInt($("input#initialDeposit").val());
+    $("#nameInitial").hide();
+    $("#depositWithdrawal").show();
+
+    return account = new NewAccount(name, balance);
+  });
+  $(".depositWithdrawal").submit(function(event) {
+    event.preventDefault();
+    var deposit = parseInt($("#deposit").val());
+    var withdrawal = parseInt($("#withdrawal").val());
+    account.loseMoney(withdrawal);
+    account.newMoney(deposit);
+    console.log(account);
+    $(".showBalance").text(account.balance);
+    $("#balanceShown").show("slow");
+  });
 });
